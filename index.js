2d7b4e68fdcc5deb2cdd9bde6b70d10841b78490
batoul sudki tenbakjy <batoul.tenbakji@gmail.com>
2022-10-27 16:02:44 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const m= document.getElementById('main');
+m.remove()
+const newHeader=document.createElement('h1')
+newHeader.setAttribute('id','victory')
+newHeader.textContent="Batoul is the champion"
+document.body.append(newHeader)
